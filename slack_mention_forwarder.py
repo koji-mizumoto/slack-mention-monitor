2b83e58a07bfd8520af98e0d7c3431bf8901d4ea
mizumoto-at-329429501674 <mizumoto@cocoroto.net>
2024-11-01 23:49:42 +0900

    minor change

--- slack_mention_forwarder.py
+++ slack_mention_forwarder.py
@@ -8,12 +8,6 @@ from datetime import datetime
 # Flaskアプリケーションの初期化
 flask_app = Flask(__name__)
 
-# Slackアプリケーションの初期化
-app = App(
-    token=os.environ["SLACK_BOT_TOKEN"],
-    signing_secret=os.environ["SLACK_SIGNING_SECRET"]
-)
-
 # Slackハンドラーの初期化
 handler = SlackRequestHandler(app)
 
